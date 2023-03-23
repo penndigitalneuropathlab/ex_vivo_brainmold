5ce59993c2ac06debfde722b87e2a08074091b04
Noah Capp <noahmcapp@gmail.com>
2023-03-23 12:47:13 -0400

    rebuilt brainmold to fix missing dots issue

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo cp ./vtklevelset /usr/local/bin/
+sudo chmod a-w /usr/local/bin/vtklevelset
+sudo chmod a+x /usr/local/bin/vtklevelset
+
+sudo cp ./brainmold /usr/local/bin/
+sudo chmod a-w /usr/local/bin/brainmold
+sudo chmod a+x /usr/local/bin/brainmold
+
+sudo cp ./brainmold_run /usr/local/bin/
+sudo chmod a-w /usr/local/bin/brainmold_run
+sudo chmod a+x /usr/local/bin/brainmold_run
+
+sudo cp ./slab_postproc /usr/local/bin/
+sudo chmod a-w /usr/local/bin/slab_postproc
+sudo chmod a+x /usr/local/bin/slab_postproc
